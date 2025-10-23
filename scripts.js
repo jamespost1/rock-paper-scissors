715c58f1ee5733374cdacdc642c1cc404e129e96
James Post <jamesbrendanpost@gmail.com>
2025-10-23 14:10:40 -0400

    Fix playGame() and getHumanChoice() logic

--- scripts.js
+++ scripts.js
@@ -1,19 +1,32 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     let val = Math.trunc((Math.random()*10)%3);
-    if(val==0){
+    if(val===0){
         return "Rock";
-    } else if (val==1){
+    } else if (val===1){
         return "Paper";
-    } else if(val==2){
+    } else if(val===2){
         return "Scissors";
     }
 }
 
 function getHumanChoice(){
     let choice = prompt("Rock, Paper, Scissors says SHOOT! Input your move:");
-    while(choice.toLowerCase!="rock"||choice.toLowerCase!="paper"||choice.toLowerCase!="scissors"){
-        prompt("Please input a valid move: Rock | Paper | Scissors");
+    
+    if(choice===null){
+        console.log("Game canceled")
     }
+    choice = choice.trim().toLowerCase();
+
+    while (choice !== "rock" && choice !== "paper" && choice !== "scissors"){
+        choice = prompt("Please input a valid move: Rock | Paper | Scissors");
+        if (choice === null) return null;
+        choice = choice.trim().toLowerCase();
+    }
+
+    return choice;
 }
 
 function playRound(humanChoice, computerChoice){
@@ -41,14 +54,14 @@ function playRound(humanChoice, computerChoice){
 }
 
 function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
     for(let i=0; i<5; i++){
+        const humanSelection = getHumanChoice();
+        if(humanSelection===null){
+            console.log("Game ended early");
+            return;
+        }
+        const computerSelection = getComputerChoice();
         playRound(humanSelection, computerSelection);
-        humanSelection = getHumanChoice();
-        computerSelection = getComputerChoice();
     }
     if(humanScore>computerScore){
         console.log("You won in a best of 5!")
@@ -56,3 +69,6 @@ function playGame(){
         console.log("You lost in a best of 5.")
     }
 }
+
+
+playGame();
